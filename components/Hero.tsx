b3176e98c62007c24f7a3bb0fda3091280999654
CkBeam <113032974+CkBeam@users.noreply.github.com>
2023-11-05 13:52:37 -0500

    update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,15 +14,16 @@ const Hero = ({ image, altText, title, description }: HeroProps) => {
 			<Image
 				src={image}
 				alt={altText}
-				sizes='(max-width: 768px) 100vw'
+				sizes='(max-width: 860px) 100vw'
 				fill={true}
 				priority={true}
 				className='absolute inset-0 h-full w-full z-0 object-cover'
 				quality={100}
 			/>
-			<div className='absolute top-3/4 left-3/4 transform -translate-x-1/2 -translate-y-1/2 text-white'>
-				<h1 className='text-4xl font-bold'>{title}</h1>
-				<p className='mt-2 text-lg'>{description}</p>
+			<div className='absolute top-1/4 left-1/2 md:top-3/4 md:left-3/4 transform -translate-x-1/2 -translate-y-1/2 text-white'>
+				<h1 className='text-sm md:text-lg lg:text-4xl mb-2 font-bold'>{title}</h1>
+				<hr className='border-t border-white w-3/4 sm:w-full' />
+				<p className='mt-2 text-sm sm:text-lg'>{description}</p>
 			</div>
 		</div>
 	);
