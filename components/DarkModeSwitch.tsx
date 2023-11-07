13cf06ad8e515fd97713d9bd59f46f9156a6d470
CkBeam <113032974+CkBeam@users.noreply.github.com>
2023-11-06 20:30:25 -0500

    added symbol to toggle

--- components/DarkModeSwitch.tsx
+++ components/DarkModeSwitch.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
+import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
@@ -29,11 +30,15 @@ export default function DarkModeSwitch() {
 	useEffect(() => {
 		checkIsDark();
 	}, [isDark]);
-
+	const symbol = isDark ? (
+		<Moon className='h-[1.2rem] w-[1.2rem] -rotate-80 scale-0 transition-all dark:scale-100' />
+	) : (
+		<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+	);
 	return (
 		<div className='flex items-center space-x-2'>
 			<Switch id='dark-mode-switch' checked={isDark} onCheckedChange={() => toggleDark()} />
-			<Label htmlFor='dark-mode-switch'>Dark Mode</Label>
+			<Label htmlFor='dark-mode-switch'>{symbol}</Label>
 		</div>
 	);
 }
