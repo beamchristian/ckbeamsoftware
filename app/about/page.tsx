ec06f826755a6ae416f196c12abbf8622ac558f4
Christian Beam <beamchristian@yahoo.com>
2023-11-20 12:47:34 -0500

    removed about and contact temporarly

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function About() {
-	return (
-		<div className='flex flex-col items-center justify-center h-screen'>
-			<h1>[About Page]</h1>
-		</div>
-	);
-}
--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Contact() {
-	return (
-		<div className='flex flex-col items-center justify-center h-screen'>
-			<h1>[Contact Page]</h1>
-		</div>
-	);
-}
