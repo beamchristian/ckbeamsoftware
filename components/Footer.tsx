c9a7735cf706c345b2e0694448b4bd30b99265e6
Christian Beam <beamchristian@yahoo.com>
2023-11-20 12:51:43 -0500

    remove links from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,10 @@ export default async function Footer() {
   return (
     <footer className="p-8 text-center bottom-0 border-t-2">
       <p>&copy; 2023 CkBeam Software</p>
-      <p>
+      {/* <p>
         <Link href="/about">About</Link> | {''}
         <Link href="/contact">Contact Us</Link>
-      </p>
+      </p> */}
     </footer>
   );
 }
