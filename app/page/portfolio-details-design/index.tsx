966befddb59193732611c649ff90b77c7498d0b1
Christian Beam <beamchristian@yahoo.com>
2025-04-15 13:21:14 -0400

    more suggestions

--- app/page/portfolio-details-design/index.tsx
+++ app/page/portfolio-details-design/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { PortfolioDataType } from "@/Types/Types";
 import { Hind } from "next/font/google";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Previous from "./previous-svg";
