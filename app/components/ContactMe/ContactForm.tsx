a0b03331a6c021e0ce29c9c43c2968032c6897d5
Christian Beam <beamchristian@yahoo.com>
2025-04-28 22:16:58 -0400

    removed unused import

--- app/components/ContactMe/ContactForm.tsx
+++ app/components/ContactMe/ContactForm.tsx
@@ -10,7 +10,7 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Hind } from "next/font/google";
 
-import { z, ZodIssue } from "zod"; // Zod for schema validation
+import { z } from "zod"; // Zod for schema validation
 
 const hind = Hind({
   subsets: ["latin"],
